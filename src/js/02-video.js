74a4348618f03a39594bbc6a5532f6f3099df618
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-05-02 21:57:26 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorageService from './localStorage';
 
+const KEY_LOCAL_STORAGE = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const { save, load } = localStorageService;
+
+const onSendUpdatePlaybackTimeInlocalStorage = e => {
+  const currentTimePlayback = e.seconds;
+  save(KEY_LOCAL_STORAGE, currentTimePlayback);
+};
+
+player.on('timeupdate', throttle(onSendUpdatePlaybackTimeInlocalStorage, 1000));
+
+player.setCurrentTime(load(KEY_LOCAL_STORAGE));
